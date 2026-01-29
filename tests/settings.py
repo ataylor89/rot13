81d189e887589622402514fef81884a6d70beb4b
Andrew Taylor <rolyata47@gmail.com>
2026-01-29 02:02:17 -0600

    Renaming variable

--- tests/settings.py
+++ tests/settings.py
@@ -1,4 +1,4 @@
 from pathlib import Path
 
 project_root = Path(__file__).parent.parent
-test_data_directory = project_root / 'tests' / 'test_data'
+test_data_path = project_root / 'tests' / 'test_data'
--- tests/test_rot13.py
+++ tests/test_rot13.py
@@ -1,13 +1,13 @@
 from rot13 import rot13
-from settings import test_data_directory
+from settings import test_data_path
 from unittest import TestCase
 
 class TestRot13(TestCase):
 
     def test_rot13(self):
-        with open(test_data_directory / 'input.txt', 'r') as file:
+        with open(test_data_path / 'input.txt', 'r') as file:
             input = file.read()
-        with open(test_data_directory / 'expected.txt', 'r') as file:
+        with open(test_data_path / 'expected.txt', 'r') as file:
             expected = file.read()
         assert rot13(input) == expected
         assert rot13(expected) == input
