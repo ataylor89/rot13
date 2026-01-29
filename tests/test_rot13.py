ad28834da5956440eb6459726fb5be25ae0c3397
Andrew Taylor <rolyata47@gmail.com>
2026-01-29 05:01:02 -0600

    Simplifying unit test

--- tests/test_rot13.py
+++ tests/test_rot13.py
@@ -1,13 +1,12 @@
 from rot13 import rot13
-from tests import test_data_path
 from unittest import TestCase
 
 class TestRot13(TestCase):
 
     def test_rot13(self):
-        with open(test_data_path / 'input.txt', 'r') as file:
+        with open('tests/test_data/input.txt', 'r') as file:
             input = file.read()
-        with open(test_data_path / 'expected.txt', 'r') as file:
+        with open('tests/test_data/expected.txt', 'r') as file:
             expected = file.read()
         assert rot13(input) == expected
         assert rot13(expected) == input
