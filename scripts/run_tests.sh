0613fc7c65660ead247b1a5bd962132d9109d1e6
Andrew Taylor <rolyata47@gmail.com>
2026-01-29 18:40:17 -0600

    Improving unit tests

---
+++ scripts/run_tests.sh
@@ -0,0 +1,2 @@
+export PYTHONPATH="$HOME/Github/rot13:$HOME/Github/rot13/src:$PYTHONPATH"
+python3 -m unittest discover -s tests -v
--- tests/test_rot13.py
+++ tests/test_rot13.py
@@ -1,12 +1,18 @@
+from tests import project_root
 from rot13 import rot13
 from unittest import TestCase
 
 class TestRot13(TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        input_path = project_root / 'tests' / 'test_data' / 'input.txt'
+        with open(input_path, 'r') as file:
+            cls.input = file.read()
+        expected_path = project_root / 'tests' / 'test_data' / 'expected.txt'
+        with open(expected_path, 'r') as file:
+            cls.expected = file.read()
+
     def test_rot13(self):
-        with open('tests/test_data/input.txt', 'r') as file:
-            input = file.read()
-        with open('tests/test_data/expected.txt', 'r') as file:
-            expected = file.read()
-        assert rot13(input) == expected
-        assert rot13(expected) == input
+        assert rot13(self.input) == self.expected
+        assert rot13(self.expected) == self.input
